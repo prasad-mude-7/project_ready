78b006f379885e0506e5d8d211efe1b3a5d8361f
prasad-mude-au9 <prasadmude7@gmail.com>
2021-04-01 20:42:38 +0530

    committed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // React build output folder
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
